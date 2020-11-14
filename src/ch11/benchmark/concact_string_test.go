412c380445d8438f1b9a317f1f8ad3dadea8c7d8
xiaolinzi-xl <daoshangxl@163.com>
2020-11-14 13:10:36 +0800

    测试

---
+++ src/ch11/benchmark/concact_string_test.go
@@ -0,0 +1,53 @@
+package benchmark
+
+import (
+	"bytes"
+	ass "github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConcatStringByAdd(t *testing.T) {
+	assert := ass.New(t)
+	elems := []string{"1", "2", "3", "4", "5"}
+	ret := ""
+	for _, elem := range elems {
+		ret += elem
+	}
+	assert.Equal("12345", ret)
+}
+
+func TestConcatStringByBytesBuffer(t *testing.T) {
+	assert := ass.New(t)
+	var buf bytes.Buffer
+	elems := []string{"1", "2", "3", "4", "5"}
+	for _, elem := range elems {
+		buf.WriteString(elem)
+	}
+	assert.Equal("12345", buf.String())
+}
+
+func BenchmarkConcatStringByAdd(b *testing.B) {
+	elems := []string{"1", "2", "3", "4", "5"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ret := ""
+		for _, elem := range elems {
+			ret += elem
+		}
+	}
+	b.StopTimer()
+}
+
+func BenchmarkConcatStringByBytesBuffer(b *testing.B) {
+	elems := []string{"1", "2", "3", "4", "5"}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		for _, elem := range elems {
+			buf.WriteString(elem)
+		}
+	}
+	b.StopTimer()
+}
+
+// go test -bench=. -benchmem
